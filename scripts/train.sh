28468b19fd8ad5f85e87b9bcfede396d70f069a2
Zach G <zehaog@cs.cmu.edu>
2019-10-02 12:35:25 -0400

    modify train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -13,7 +13,7 @@ work_dir="work_dir"
 mkdir -p ${work_dir}
 echo save results to ${work_dir}
 
-python nmt.py \
+python3 nmt.py \
     train \
     --cuda \
     --vocab ${vocab} \
@@ -23,13 +23,13 @@ python nmt.py \
     --dev-tgt ${dev_tgt} \
     --save-to ${work_dir}/model.bin \
     --valid-niter 2400 \
-    --batch-size 64 \
+    --batch-size 128 \
     --hidden-size 256 \
     --embed-size 256 \
     --uniform-init 0.1 \
     --dropout 0.2 \
     --clip-grad 5.0 \
-    --lr-decay 0.5 2>${work_dir}/err.log
+    --lr-decay 1e-6 2>${work_dir}/err.log
 
 python nmt.py \
     decode \
